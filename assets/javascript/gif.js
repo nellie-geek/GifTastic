b2451d02c060b92648533e4a762aa0ba346e36f1
nellie-geek <janellegieck@msn.com>
2019-03-12 16:45:23 -0600

    Added function and JS.

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,29 +1,48 @@
-alert("asdf")
 var fails = ["homework", "work", "winter", "snow", "walking", "football", "cheerleading", "parking", "driving", "running",
      "skating", "iceskating", "gymnastics"];
 
     function displayFailImg() {
     
         var fail = $(this).attr("data-name")
-        var queryURL = "https://api.giphy.com//v1/gifs/search?q=" + fail + "&limit=10&api_key=KbfjUcGQ46v2IkPygFpVdZBB2jn2cqSo";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + fail + "&limit=10&api_key=KbfjUcGQ46v2IkPygFpVdZBB2jn2cqSo";
     
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response) {
             console.log(response);
-
+            for (var i = 0; i < fails.length; i++) {
             var gifDiv = $("<div class='gifInfo'></div>");
             
-            var rating= response.data[i].rating;
+            var rating = response.data[i].rating;
             var ratingText = $("<p>").text("Rating: " + rating);
             gifDiv.append(ratingText);
 
-            })
-            
+            var stillURL = response.data[i].images.fixed_height_still.url;
+            var animateURL = response.data[i].images.fixed_height.url;
+            var gifImage = $("<img>");
+        
+            gifImage.attr("src", stillURL);
+            gifImage.attr("data-still", stillURL);
+            gifImage.attr("data-animate", animateURL);
+            gifImage.attr("data-state", "still");
+            gifImage.addClass("gif");
 
-                //add still vs animate 
+            $("#gif-view").prepend(gifDiv, gifImage); 
         }
+        });    
+    };
+
+        $("#gif-view").on("click", ".gif", function (){
+            var state = $(this).attr("data-state");
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+              } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+              }
+        });
 
     function renderButtons() {
         $("#button-view").empty();
