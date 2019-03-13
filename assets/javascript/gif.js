0e5ae3ee541ebf479569d6963718c381ed4c7618
nellie-geek <janellegieck@msn.com>
2019-03-12 19:13:28 -0600

    Updated JS and started styles - css not linked.

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,16 +1,17 @@
-var fails = ["homework", "work", "winter", "snow", "walking", "football", "cheerleading", "parking", "driving", "running",
-     "skating", "iceskating", "gymnastics"];
+    //array of fails
+var fails = ["winter", "snow", "walking", "football", "cheerleading", "parking", "driving", "running",
+     "skating", "gymnastics"];
 
     function displayFailImg() {
     
         var fail = $(this).attr("data-name")
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + fail + "&limit=10&api_key=KbfjUcGQ46v2IkPygFpVdZBB2jn2cqSo";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=fails+" + fail + "&limit=10&api_key=KbfjUcGQ46v2IkPygFpVdZBB2jn2cqSo";
     
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response) {
-            console.log(response);
+        
             for (var i = 0; i < fails.length; i++) {
             var gifDiv = $("<div class='gifInfo'></div>");
             
@@ -46,9 +47,9 @@ var fails = ["homework", "work", "winter", "snow", "walking", "football", "cheer
 
     function renderButtons() {
         $("#button-view").empty();
-            console.log("hi");
+        
         for (var i = 0; i < fails.length; i++) {
-            console.log("hi");
+           
             var a = $("<button>");
             a.addClass("fail");
             a.attr("data-name", fails[i]);
